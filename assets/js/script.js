519999304046eb95a32c63f41bb63477c7f7edf6
Andrewy2416 <andrewy2416@gmail.com>
2021-11-02 15:37:30 -0700

    added local storage for list of previously serched cities and function for returning main info sch as temperature, humidity, and wind speed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
 //placeholder for API
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+const apiKey = "";
+var currWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#weatherForecast");
+var citiesArray;
+
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
+
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let currTime = new Date(response.dt*1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp} </p>
+        <p>Humidity: ${response.main.humidity} </p>
+        <p>Wind Speed: ${response.wind.speed} </p>
+        `, returnUVIndex(response.coord))
+        createHistoryButton(response.name);
+    })
+};
